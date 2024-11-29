d89e97807eb0439c9b0dd862ea7c434feb3157eb
nandaditra <nandaditra56@gmail.com>
2024-11-29 19:49:49 +0700

    add network component

---
+++ src/components/NetworkError/index.tsx
@@ -0,0 +1,11 @@
+const NetworkError = () => {
+    return (
+        <div className="container">
+           <div className="mx-auto">
+             <h1>No Connection</h1>
+           </div>
+        </div>
+       )
+}
+
+export default NetworkError
