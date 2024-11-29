3568130929a23eff82506c1f18697d2421284d5c
nandaditra <nandaditra56@gmail.com>
2024-11-29 19:47:35 +0700

    add Browser Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 import Router from './router';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-   <Router />
+    <BrowserRouter>
+       <Header />
+         <Router />
+       <Footer />
+    </BrowserRouter>
   );
 }
 
